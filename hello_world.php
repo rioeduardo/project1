55efd18d0f3ecc972481efcc274ebe1ef65b1a96
Rio Eduardo Bangkit Gideon Simatupang <rioeduardo92@gmail.com>
2014-05-12 22:44:02 +0700

    add 2

--- hello_world.php
+++ hello_world.php
@@ -4,11 +4,5 @@
 	echo "ok";
 	echo "update now";
 
-<<<<<<< HEAD
-	echo "new update available!2";
-=======
 	echo "new update available!1";
->>>>>>> 2c66c94497ed665b2006d12601aeeeaac008d0b5
-
-	echo "same testing!";
 ?>
